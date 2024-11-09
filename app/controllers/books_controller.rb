0185a6cdb3b712b6774334df72dc2a2b4876c095
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-09 22:54:15 +0900

    setting_create_action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,16 @@
 class BooksController < ApplicationController
   def index
-
   end
 
   def new
     @book = Book.new
+  end
+
+  def create
+    Book.create(book_params)
+  end
+  private
+  def book_params
+    params.require(:book).permit(:name,:text,:image)
   end
 end
